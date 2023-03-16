881bcdb8831edf9160cdc5fc06f6f96a4a25c5ce
Imran <imranmansuri038@gmail.com>
2023-03-16 18:40:19 +0530

    HomeSearch added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,23 @@
 import HomeHeader from "@/components/HomeHeader";
+import Image from "next/image";
+import HomeSearch from "@/components/HomeSearch";
 
 const Home = () => {
   return (
     <>
+      {/* Header */}
       <HomeHeader />
+
+      {/* body  */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          width="300"
+          height="100"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+        />
+
+        <HomeSearch />
+      </div>
     </>
   );
 };
