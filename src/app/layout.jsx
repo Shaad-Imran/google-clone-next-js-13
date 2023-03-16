b302efa8eb20ec5446e5310f5d736c1e958f819b
Imran <imranmansuri038@gmail.com>
2023-03-16 16:15:44 +0530

    install tailwind css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,5 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "Google Clone",
-  description: "Google Clone created by Next js 13",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <div>Home</div>;
+  return <div className=" text-red-600 font-serif text-5xl">Home</div>;
 };
 
 export default Home;
