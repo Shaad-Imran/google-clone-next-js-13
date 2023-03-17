05a6cf81b7be614d0875f8d6b2e3e2dc4d992437
Imran <imranmansuri038@gmail.com>
2023-03-17 16:57:35 +0530

    footer added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,6 +14,7 @@ const Home = () => {
           width="300"
           height="100"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+          alt="google logo"
         />
 
         <HomeSearch />
